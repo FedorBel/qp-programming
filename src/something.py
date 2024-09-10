0af05b81a79aeab8dcdec16a07cea877cadb8af8
Fedor <belyakovfyu@gmail.com>
2024-09-10 10:22:32 +0300

    boom

--- src/something.py
+++ src/something.py
@@ -3,8 +3,13 @@ import scipy as sp
 from scipy import sparse
 
 
-a = np.array([[1, 0], [2, 0], [0, 3]])
-b = np.array([5, 6, 7])
-print(a)
-print(b)
-print(a + b.T)
+# a = np.array([[1, 0], [2, 0], [0, 3]])
+# b = np.array([5, 6, 7])
+
+# print(sparse.eye(1))
+
+q1 = np.array([2, 0.5])
+q1 = q1 / np.linalg.norm(q1)
+q2 = np.array([-0.5, 2])
+q2 = q2 / np.linalg.norm(q2)
+print(q1.T @ q2)
